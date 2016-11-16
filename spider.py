e326f59760bdd0ca02346a7195ed2a02f7d7dd33
xiaoshenke <1205914137@qq.com>
2016-11-16 20:51:53 +0800

    add code

--- spider.py
+++ spider.py
@@ -2,6 +2,7 @@
 
 import requests
 import re
+import json
 from lxml import html
 from db import ZhihuUserProfile
 import sys
@@ -15,12 +16,11 @@ except:
 
 
 class ZhihuSpider():
-	''' 计划存入的信息包括 该用户的大学信息 性别 关注者数 被关注者数 获得的赞 分享 收藏等 供后续进行数据分析
-	'''
 	people_url = 'https://www.zhihu.com/people/'
 	followee = '/followees'
-	grabbed_id = []  #grabbed userid 
+	grabbed_id = []  
 	spector = '话题优秀回答者'
+	current_grab_id = -1
 
 	def get_self_userid(self):
 		url = "https://www.zhihu.com/settings/profile"
@@ -47,10 +47,6 @@ class ZhihuSpider():
 			return ''
 
 	def __init__(self,headers,cookies):
-		''' 先从followee开始爬某个关注者是否是某个领域的优秀回答者 比如python。然后遍历该用户的关注里的对应
-		领域的优秀回答者。广度遍历，人数达到500或者遍历完所有优秀回答者视为成功。
-		若你的关注中没有该领域的回答者。让用户输入一个userid，以它作为根开始遍历
-		'''
 		self.headers = headers
 		self.cookies = cookies
 		self.rootid = self.get_self_userid()
@@ -61,7 +57,8 @@ class ZhihuSpider():
 	# grab https://www.zhihu.com/people/userid/followees page from internet
 	# 若是第一个(第三个参数为true) 由于是整个爬虫的root。该root用户的信息不存入数据库
 	# 否则存入数据库
-	def grab_from_id(self,id,first):
+	def grab_from_currentid(self,first):
+		id = self.current_grab_id
 		url = self.people_url+'%s'%id+self.followee
 		try:
 			r = requests.get(url,cookies=self.cookies,headers=self.headers,verify=False)
@@ -75,31 +72,54 @@ class ZhihuSpider():
 		self.parse_user_profile(content,first)
 		return 
 		
-	# Todo
  	def deal_followee_node(self,node):
- 		name = node.xpath("//span[@class='author-link-line']//a[@class='zg-link author-link']")[0].text
- 		link = node.xpath("//span[@class='author-link-line']//a/@href")[0]
- 		userid = re.search(r'(?<=people[/]).+',link).group(0)
+ 		print('test deal_followee_node node: %s'%node)
  		spector = node.xpath("//span[@class='badge-summary']//a")[0].text.strip()
  		if spector:
  			selfstring = self.topic.strip()+self.spector.strip()
   			if re.search(r'%s'%selfstring,spector.encode('utf-8')): #这里必须要encode成utf-8格式
- 				print('test we find a %s spector'%self.topic)
- 		return;
+  				name = node.xpath("//span[@class='author-link-line']//a[@class='zg-link author-link']")[0].text	
+ 				print('test we find a %s spector,name %s'%(self.topic,name))
+ 				link = node.xpath("//span[@class='author-link-line']//a/@href")[0]	
+ 				userid = re.search(r'(?<=people[/]).+',link).group(0)
+ 				self.grabbed_id.append(userid) # 存入抓取的id		
+ 		return
 
 	# Todo: 模拟下拉加载更多来拿到所有的关注者
 	def get_followees_from(self,html_source):
+		print('test get_followees_from %s'%html_source)
 		tree = html.fromstring(html_source)
 		followee_num = tree.xpath("//div[@class='zu-main-sidebar']//strong")[0].text
 		for i in xrange((int(followee_num) - 1) / 20 + 1):
 			if i == 0:
-				#TODO
+				continue
 				for node in tree.xpath("//div[@class='zm-list-content-medium']"):
 					self.deal_followee_node(node)
-					break
 			else:
-				break
-				#TODO
+				post_url = "http://www.zhihu.com/node/ProfileFolloweesListV2"
+                _xsrf = tree.xpath("//input[@name='_xsrf']/@value")[0]
+                print('test _xsrf is %s'%_xsrf)
+                offset = i * 20
+                hash_id = re.findall("hash_id&quot;: &quot;(.*)&quot;},", html_source)[0]
+                print('test hash_id %s'%hash_id)
+                params = json.dumps({"offset": offset, "order_by": "created", "hash_id": hash_id})
+                data = {
+                    '_xsrf': _xsrf,
+                    'method': "next",
+                    'params': params
+                }
+                id = self.current_grab_id
+                referer_url = self.people_url+'%s'%id+self.followee
+                header = {
+                	'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
+                	'Host': "www.zhihu.com",
+                	'Referer': referer_url
+                }
+                r_post = requests.post(post_url,data=data, headers=self.headers,verify=False)
+                print('test r_post result %s'%r_post)
+                followee_list = r_post.json()["msg"]
+                for j in xrange(min(followee_num - i * 20, 20)):
+                	self.deal_followee_node(html.fromstring(followee_list[j])) #FIXME: mabi被封了...
 		return
 
 	#parse user profile
@@ -111,10 +131,11 @@ class ZhihuSpider():
 	def grab_topic(self,topic):
 		current_index = 0
 		while (current_index < len(self.grabbed_id) and current_index < 50):
+			self.current_grab_id = self.grabbed_id[current_index]
 			if current_index ==0:
-				self.grab_from_id(self.grabbed_id[current_index],True)
+				self.grab_from_currentid(True)
 			else:
-				self.grab_from_id(self.grabbed_id[current_index],False)
+				self.grab_from_currentid(False)
 			current_index = current_index+1
 		if len(self.grabbed_id) == 1:
 			return
